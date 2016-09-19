247c9190bb0a52e1b41574f8077aaaa4e6b25214
Sora Malka <smargoli2@student.touro.edu>
2016-09-19 17:58:36 -0400

    Added Adder() class

---
+++ src/Adder.java
@@ -0,0 +1,8 @@
+
+public class Adder {
+
+	public static Integer Add(Integer a, Integer b){
+		Integer sum = a + b;
+		return sum;
+	}
+}
--- src/Hello.java
+++ src/Hello.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+
 public class Hello {
 	public static void main(String[] args) {
-		
-Scanner keyboard = new Scanner(System.in);
+
+		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter one number followed by another to get their sum.");
 		Integer num1 = keyboard.nextInt();
 		System.out.println("Enter the second number.");
 		Integer num2 = keyboard.nextInt();
+
+		Integer sum = Adder.Add(num1, num2);
+		System.out.println("The sum is " + sum);
 		
-		Integer total = num1 + num2;
-		System.out.println("The total is " + total);
+		keyboard.close();
 	}
 
 }
