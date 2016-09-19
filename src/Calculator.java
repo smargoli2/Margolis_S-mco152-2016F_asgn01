77840533e2463650d6810668b9da0f2e6214803e
Sora Malka <smargoli2@student.touro.edu>
2016-09-19 18:23:59 -0400

    Changed to general calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+
+public class Calculator {
+
+	public static Integer Add(Integer a, Integer b){
+		Integer sum = a + b;
+		return sum;
+	}
+	
+	public static Integer subtract(Integer a, Integer b){
+		Integer difference = a - b;
+		return difference;
+	}
+}
--- src/Hello.java
+++ src/Hello.java
@@ -4,16 +4,32 @@ public class Hello {
 	public static void main(String[] args) {
 
 		Scanner keyboard = new Scanner(System.in);
-		System.out.println("Enter one number followed by another to get their sum.");
-		Integer num1 = keyboard.nextInt();
-		System.out.println("Enter the second number.");
-		Integer num2 = keyboard.nextInt();
+		int userChoice = keyboard.nextInt();
+		do {
+			System.out.println("You can either add or subtract 2 numbers. "
+					+ "This is a VERY sophiticated program. Enter 1 to add, 2 to subtract or 3 to exit.");
+			userChoice = keyboard.nextInt();
+			if (userChoice == 1) {
+				System.out.println("Enter the first number.");
+				Integer num1 = keyboard.nextInt();
+				System.out.println("Enter the second number.");
+				Integer num2 = keyboard.nextInt();
+				Integer sum = Calculator.Add(num1, num2);
+				System.out.println("The sum is " + sum);
+			} else if (userChoice == 2) {
+				System.out.println("Enter the first number.");
+				Integer num1 = keyboard.nextInt();
+				System.out.println("Enter the second number.");
+				Integer num2 = keyboard.nextInt();
+				Integer diff = Calculator.subtract(num1, num2);
+				System.out.println("The difference is " + diff);
+			} else {
+				System.out.println("Goodbye!");
+				System.exit(0);
+				;
+			}
+		} while (userChoice == 1 || userChoice == 2);
 
-		Integer sum = Adder.Add(num1, num2);
-		System.out.println("The sum is " + sum);
-		
-		Integer diff = Subtractor.subtract(num1, num2);
-		System.out.println("The difference is " + diff);
 		keyboard.close();
 	}
 
